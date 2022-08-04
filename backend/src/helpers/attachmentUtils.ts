61f8f891f296c6ddcdadc47445d9c3de89cf4778
Clifford Usidamen <cliffordusidamen@gmail.com>
2022-08-04 23:30:56 +0100

    Complete project

---
+++ backend/src/helpers/attachmentUtils.ts
@@ -0,0 +1,26 @@
+import * as AWS from 'aws-sdk'
+import { Types } from 'aws-sdk/clients/s3';
+// import * as AWSXRay from 'aws-xray-sdk'
+import { createLogger } from '../utils/logger'
+
+const logger = createLogger('TodosAccess')
+// const XAWS = AWSXRay.captureAWS(AWS)
+
+// TODO: Implement the fileStogare logic
+export class AttachmentUtils {
+
+    static async generateUploadUrl(todoId: string): Promise<string> {
+
+        const s3Client: Types = new AWS.S3({ signatureVersion: 'v4' })
+    
+        const url = s3Client.getSignedUrl('putObject', {
+            Bucket: process.env.ATTACHMENT_S3_BUCKET,
+            Key: todoId,
+            Expires: 1000,
+        });
+
+        logger.log('info', `Generating S3 bucket URL: ${url}`);
+    
+        return url as string;
+    }
+}
